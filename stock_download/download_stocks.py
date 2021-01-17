f1933a85638bee8151fa7a2e261c7f93be5200cc
Juan Carlos <juancar.hernan@hotmail.com>
2021-01-17 00:22:28 -0600

    Stock Function

--- stock_download/download_stocks.py
+++ stock_download/download_stocks.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
+import yfinances as yf
 
+def DownloadStockInformation(stocks, start, end):
+    if not isinstance(stocks, list) or not isinstance(stocks, str): raise Exception('"stocks" should be a list or a string')
+    if not isinstance(start, str): raise Exception('"start" should be a string')
+    if not isinstance(end, str): raise Exception('"end" should be a string')
+    data = yf.download(stocks, start=start, end=end)
+    return data
